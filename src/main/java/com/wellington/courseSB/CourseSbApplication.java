608ddbc33eac0933bca6f32e55658381be23c6cf
Wellington Souza <wel.souza88@gmail.com>
2023-03-17 21:01:47 -0300

    Project created

---
+++ src/main/java/com/wellington/courseSB/CourseSbApplication.java
@@ -0,0 +1,13 @@
+package com.wellington.courseSB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseSbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseSbApplication.class, args);
+	}
+
+}
