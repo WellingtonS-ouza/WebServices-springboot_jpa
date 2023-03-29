732b6a0998de5a05391404731a0f391b1b361ee1
Wellington Souza <wel.souza88@gmail.com>
2023-03-29 17:23:19 -0300

    Payment, one-to-one association

--- src/main/java/com/wellington/courseSB/config/TestConfig.java
+++ src/main/java/com/wellington/courseSB/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.wellington.courseSB.entities.Category;
 import com.wellington.courseSB.entities.Order;
 import com.wellington.courseSB.entities.OrderItem;
+import com.wellington.courseSB.entities.Payment;
 import com.wellington.courseSB.entities.Product;
 import com.wellington.courseSB.entities.User;
 import com.wellington.courseSB.entities.enums.OrderStatus;
@@ -83,6 +84,11 @@ public class TestConfig implements CommandLineRunner {
 
 
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4) );
+		
+		Payment pay1 =  new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 	}
 
 }
